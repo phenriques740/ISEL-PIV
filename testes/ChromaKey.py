4a6d408ca37dd60038f151b4c2707b2373ad336e
Pedro Henriques <35275083+eddoasso@users.noreply.github.com>
2020-10-27 12:15:42 +0000

    versao final de chromakey

--- testes/ChromaKey.py
+++ testes/ChromaKey.py
@@ -21,30 +21,77 @@ mask = cv2.imread(maskPath)
 background = cv2.imread(backgroundPath)
 
 
-newImg = cv2.bitwise_and(img,mask) + cv2.bitwise_and(background,cv2.bitwise_not(mask))
+newImg = img
+
+def apply_mask():
+    
+    
+    falcon = cv2.bitwise_and(img,mask)
+    back = cv2.bitwise_and(background,cv2.bitwise_not(mask))
+    newImg =  falcon + back
 
 
-cv2.imshow('originalImg',img)
-cv2.imshow('mask',mask)
-
-cv2.imshow('background',background)
-
-cv2.imshow('Chroma key img', newImg)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-cv2.imshow('originalImg',newImg)
+    cv2.imshow('falcon' , falcon)
+    cv2.imshow('back' , back)
+    cv2.imshow('Chroma key img', newImg)
+    
+    cv2.imwrite(imgDir + 'processedImg/bitwise_mask.png' , newImg)
 
 
-blurImg = cv2.blur(newImg, (10,10))
-cv2.imshow('blurred Img' , blurImg)
+def mask2():
+    global newImg
+    falcon = cv2.multiply(img, mask, scale=1.0/255)
+    back = cv2.multiply(background, 255 - mask, scale=1.0/255)
+    newImg = cv2.add(falcon, back)
+    
+    
+    cv2.imshow('falcon',falcon)
+    cv2.imshow('back' , back)
+    cv2.imshow('chroma key 2' , newImg)
 
-medianBlur = cv2.medianBlur(newImg, 9) # perguntar porque valor de k % 2 tem de ser 1
-cv2.imshow('medianBlur' , medianBlur)
+    cv2.imwrite(imgDir + 'processedImg/scalar_mask.png' , newImg)
 
-gausianBlur = cv2.GaussianBlur(newImg, (5,5), 0)
-cv2.imshow('gausianBlur' , gausianBlur)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def blurring():
+    cv2.imshow('originalImg',newImg)
+
+    blurImg = cv2.blur(newImg, (10,10))
+    cv2.imshow('blurred Img' , blurImg)
+    
+    medianBlur = cv2.medianBlur(newImg, 9) # perguntar porque valor de k % 2 tem de ser 1
+    cv2.imshow('medianBlur' , medianBlur)
+    
+    gausianBlur = cv2.GaussianBlur(newImg, (5,5), 0)
+    cv2.imshow('gausianBlur' , gausianBlur)
+
+
+def trans_geo():
+    img = newImg
+    print('show')
+    rot_mat = cv2.getRotationMatrix2D((img.shape[1]/2, img.shape[0]/2),30,1)
+    print('mat')    
+    rot_dst = cv2.warpAffine(img, rot_mat, (img.shape[1], img.shape[0]))
+    print('rot')
+    cv2.imshow('Source image', img)
+    cv2.imshow('Rotate', rot_dst)
+    
+    
+try:
+    apply_mask()
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    mask2()
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+    trans_geo()
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+    
+except Exception as e:
+    print(e)
+    cv2.destroyAllWindows()
