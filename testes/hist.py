ba21a691f5a1f48b23edc2dfea02fbfecc62ca20
Pedro Henriques <35275083+eddoasso@users.noreply.github.com>
2020-11-02 15:27:56 +0000

    ficheiro histograma criado, faz o histograma de cores de uma imagem

---
+++ testes/hist.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  2 14:24:42 2020
+
+@author: pedro
+"""
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+imgDir = '../imageDatabase/'
+imgPath = imgDir+'lily-lotus-flowers.jpg'
+
+src = cv.imread(imgPath)
+
+
+bgr_planes = cv.split(src)
+histSize = 256
+histRange = (0, 256) # the upper boundary is exclusive
+accumulate = False
+b_hist = cv.calcHist(bgr_planes, [0], None, [histSize], histRange, accumulate=accumulate)
+g_hist = cv.calcHist(bgr_planes, [1], None, [histSize], histRange, accumulate=accumulate)
+r_hist = cv.calcHist(bgr_planes, [2], None, [histSize], histRange, accumulate=accumulate)
+hist_w = 512
+hist_h = 400
+bin_w = int(round( hist_w/histSize ))
+histImage = np.ones((hist_h, hist_w, 3), dtype=np.uint8)*255
+cv.normalize(b_hist, b_hist, alpha=0, beta=hist_h, norm_type=cv.NORM_MINMAX)
+cv.normalize(g_hist, g_hist, alpha=0, beta=hist_h, norm_type=cv.NORM_MINMAX)
+cv.normalize(r_hist, r_hist, alpha=0, beta=hist_h, norm_type=cv.NORM_MINMAX)
+for i in range(1, histSize):
+    cv.line(histImage, ( bin_w*(i-1), hist_h - int(np.round(b_hist[i-1])) ),
+            ( bin_w*(i), hist_h - int(np.round(b_hist[i])) ),
+            ( 255, 0, 0), thickness=2)
+    cv.line(histImage, ( bin_w*(i-1), hist_h - int(np.round(g_hist[i-1])) ),
+            ( bin_w*(i), hist_h - int(np.round(g_hist[i])) ),
+            ( 0, 255, 0), thickness=2)
+    cv.line(histImage, ( bin_w*(i-1), hist_h - int(np.round(r_hist[i-1])) ),
+            ( bin_w*(i), hist_h - int(np.round(r_hist[i])) ),
+            ( 0, 0, 255), thickness=2)
+cv.imshow('Source image', src)
+cv.imshow('calcHist Demo', histImage)
+cv.waitKey()
+cv.destroyAllWindows()
